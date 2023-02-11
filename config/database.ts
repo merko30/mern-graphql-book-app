7bba90037986cf2148d24556804c8491eee5bbb6
Merim <merim.hasanbegovic@outlook.com>
2023-02-11 20:07:30 +0100

    update server packages
    Signed-off-by: Merim <merim.hasanbegovic@outlook.com>

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 export default async () => {
   try {
-    await mongoose.connect(process.env.DB!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
+    await mongoose.connect(process.env.DB!);
     console.log("Connected to database");
   } catch (error) {
     console.log("Database connection error: ", error);
--- server.ts
+++ server.ts
@@ -2,8 +2,7 @@ import "reflect-metadata";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import jwt from "express-jwt";
-import path from "path";
+import { expressjwt as jwt } from "express-jwt";
 import cors from "cors";
 
 require("dotenv").config();
@@ -33,19 +32,11 @@ import connect from "./config/database";
     jwt({
       secret: process.env.JWT_SECRET!,
       credentialsRequired: false,
-      algorithms: ["sha1", "RS256", "HS256"],
+      algorithms: ["RS256", "HS256"],
     })
   );
 
   server.applyMiddleware({ app });
-
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
-    app.get("*", (_, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
 
   const port = process.env.PORT || 4000;
 
