2ea8d09a08e8c0702700a847be435a1b4ec7eaa8
Merim <merim.hasanbegovic@outlook.com>
2025-02-20 13:51:15 +0100

    fix: issues, working version

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-// import "./tailwind.css";
+import "./tailwind.css";
 
 const container = document.getElementById("root");
 
