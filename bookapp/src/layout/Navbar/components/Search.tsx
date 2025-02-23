622476e5c50254a3a575e9ff9f44a74d5cc7778a
Merim <merim.hasanbegovic@outlook.com>
2025-02-23 16:28:12 +0100

    feat: loading search results

--- bookapp/src/layout/Navbar/components/Search.tsx
+++ bookapp/src/layout/Navbar/components/Search.tsx
@@ -1,22 +1,39 @@
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useDebounceValue } from "usehooks-ts";
+import { useDebounceValue, useOnClickOutside } from "usehooks-ts";
 
-import { useSearchQuery } from "src/generated";
+import { useSearchQuery, SearchQuery } from "src/generated";
+
+type SearchResults = Pick<SearchQuery, "search">["search"];
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [active, setActive] = useState(false);
+  const [results, setResults] = useState<SearchResults>([]);
 
-  const { refetch, ...data } = useSearchQuery({ skip: !term });
+  const ref = useRef<HTMLInputElement | null>(null);
+  const resultsRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(
+    [
+      ref as RefObject<HTMLInputElement>,
+      resultsRef as RefObject<HTMLDivElement>,
+    ],
+    () => setActive(false)
+  );
+
+  const { refetch } = useSearchQuery({ skip: !term });
 
   const [debouncedValue] = useDebounceValue(term, 1000);
 
   useEffect(() => {
-    refetch({ term: debouncedValue });
+    refetch({ term: debouncedValue }).then((res) => {
+      setResults(res?.data?.search || []);
+      setActive(true);
+    });
   }, [debouncedValue]);
-
-  console.log(data);
 
   return (
     <div className="container w-full flex-1 relative bg-white rounded-md">
@@ -27,8 +44,30 @@ const Search = () => {
           value={term}
           placeholder="Search books..."
           className="bg-transparent pl-2"
+          ref={ref as RefObject<HTMLInputElement>}
         />
       </div>
+      {active && (
+        <div
+          ref={resultsRef}
+          className="absolute top-12 w-full bg-white rounded-md shadow-md z-10"
+        >
+          {results.map((book) => (
+            <div key={book.id} className="p-2 border-b border-background">
+              <Link to={`/book/${book.id}`} onClick={() => setActive(false)}>
+                <p className="text-foreground">{book.volumeInfo.title}</p>
+                <div className="flex items-center">
+                  {book.volumeInfo.authors?.map((author) => (
+                    <span key={author} className="text-xs text-gray-400">
+                      {author}
+                    </span>
+                  ))}
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
